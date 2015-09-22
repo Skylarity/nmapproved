137eddd46c4cc9c36988def0720189b996a73b2d
Skyler Rexroad <skyrex1095@gmail.com>
2015-09-21 19:55:59 -0600

    Finished business class

--- php/classes/business.php
+++ php/classes/business.php
@@ -180,11 +180,138 @@ class Business implements JsonSerializable {
         $this->category = Filter::filterString($category, "Business category", 64);
     }
 
+    /**
+     * Implements JSON serializing for this class
+     * @return array
+     */
     public function JsonSerialize() {
         $fields = get_object_vars($this);
         return ($fields);
     }
 
-    // TODO: DB Stuff
+    /**
+     * Inserts business into MySQL
+     * @param PDO $pdo
+     */
+    public function insert(PDO &$pdo) {
+        // Verify business does not already exist
+        if($this->businessId !== null) {
+            throw new PDOException("Business already exists!");
+        }
+
+        // Create query template
+        $query = "INSERT INTO business(name, location, phone, website, email, category) VALUES (:name, :location, :phone, :website, :email, :category)";
+        $statement = $pdo->prepare($query);
+
+        // Bind the variables to the placeholders
+        $parameters = array("name" => $this->getName(), "location" => $this->getLocation(), "phone" => $this->getPhone(), "website" => $this->getWebsite(), "email" => $this->getEmail(), "category" => $this->getCategory());
+        $statement->execute($parameters);
+
+        // Update null ID with real ID
+        $this->businessId = intval($pdo->lastInsertId());
+    }
+
+    /**
+     * Deletes business from MySQL
+     * @param PDO $pdo
+     */
+    public function delete(PDO &$pdo) {
+        // Verify business is NOT NULL
+        if($this->businessId === null) {
+            throw new PDOException("Unable to delete a nonexistent business");
+        }
+
+        // Create query template
+        $query = "DELETE FROM business WHERE businessId = :businessId";
+        $statement = $pdo->prepare($query);
+
+        // Bind the variables to the placeholders
+        $parameters = array("businessId" => $this->getBusinessId());
+        $statement->execute($parameters);
+    }
+
+    /**
+     * Updates business in MySQL
+     * @param PDO $pdo
+     */
+    public function update(PDO &$pdo) {
+        // Create query template
+        $query = "UPDATE business SET name = :name, location = :location, phone = :phone, website = :website, email = :email, category = :category WHERE businessId = :businessId";
+        $statement = $pdo->prepare($query);
+
+        // Bind the variables to the placeholders
+        $parameters = array("name" => $this->getName(), "location" => $this->getLocation(), "phone" => $this->getPhone(), "website" => $this->getWebsite(), "email" => $this->getEmail(), "category" => $this->getCategory(), "businessId" => $this->getBusinessId());
+        $statement->execute($parameters);
+    }
+
+    /**
+     * @param PDO $pdo
+     * @param $businessId business ID to check for
+     * @return Business|null returns a business if found, or null
+     * @throws Exception catch-all error handling
+     */
+    public function getBusinessById(PDO &$pdo, $businessId) {
+        // Filter ID
+        $businessId = Filter::filterInt($businessId, "Business ID");
+
+        // Create query template
+        $query = "SELECT businessId, name, location, phone, website, email, category FROM business WHERE businessId = :businessId";
+        $statement = $pdo->prepare($query);
+
+        // Bind the variables to the placeholders
+        $parameters = array("businessId" => $businessId);
+        $statement->execute($parameters);
+
+        // Grab the business from MySQL
+        try {
+            $business = null;
+            $statement->setFetchMode(PDO::FETCH_ASSOC);
+            $row = $statement->fetch();
+
+            if($row !== false) {
+                $business = new Business($row["businessId"], $row["name"], $row["location"], $row["phone"], $row["website"], $row["email"], $row["category"]);
+            }
+        } catch(Exception $exception) {
+            // Rethrow to caller
+            throw new Exception($exception->getMessage(), 0, $exception);
+        }
+
+        return $business;
+    }
+
+    /**
+     * Gets a business from an attribute and a string
+     * @param PDO $pdo
+     * @param string $attribute business attribute to grab
+     * @param string $string data to check against
+     * @return SplFixedArray array of business
+     * @throws Exception catch-all error handling
+     */
+    public function getBusinessesByString(PDO &$pdo, $attribute, $string) {
+        // Filter the inputs
+        $attribute = Filter::filterString($attribute, "Input string \"attribute\" in getBusinessByString()");
+        $string = Filter::filterString($string, "Input string \"string\" in getBusinessByString()");
+
+        // Create query template
+        $query = "SELECT businessId, name, location, phone, website, email, category FROM business WHERE $attribute = $string";
+        $statement = $pdo->prepare($query);
+        $statement->execute();
+
+        // Grab the businesses from MySQL
+        $businesses = new SplFixedArray($statement->rowCount());
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        while(($row = $statement->fetch()) !== false) {
+            try {
+                $business = new Business($row["businessId"], $row["name"], $row["location"], $row["phone"], $row["website"], $row["email"], $row["category"]);
+                $businesses[$businesses->key()] = $business;
+                $businesses->next();
+            } catch(Exception $exception) {
+                // Rethrow to caller
+                throw new Exception($exception->getMessage(), 0, $exception);
+            }
+        }
+
+        return $businesses;
+    }
 
 }
