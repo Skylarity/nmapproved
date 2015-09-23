a1234d90d2ea555256a8e92479e2260732eb40ef
Charles Sandidge <sandidgec@gmail.com>
2015-09-22 23:18:14 -0600

    added admin panel (add businesses) page - added more to forms

--- pages/adminpanel.php
+++ pages/adminpanel.php
@@ -10,9 +10,30 @@
 
 		<form class="form-horizontal" role="form" method="post" action="#">
 			<div class="col-sm-6 col-sm-offset-3">
+
 				<div class="form-group">
 					<label for="Example Business">Business</label>
 					<input type="text" class="form-control" id="business" placeholder="Business">
+				</div>
+
+				<div class="form-group">
+					<label for="location">Location</label>
+					<input type="text" class="form-control" id="location" placeholder="Location">
+				</div>
+
+				<div class="form-group">
+					<label for="category">Phone</label>
+					<input type="text" class="form-control" id="phone" placeholder="Phone">
+				</div>
+
+				<div class="form-group">
+					<label for="category">Website</label>
+					<input type="text" class="form-control" id="website" placeholder="Website">
+				</div>
+
+				<div class="form-group">
+					<label for="category">Email</label>
+					<input type="email" class="form-control" id="email" placeholder="Email">
 				</div>
 
 				<div class="form-group">
