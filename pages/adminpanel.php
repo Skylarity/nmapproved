031dff42d01c018a5ee15a176b787ca3126086e7
Charles Sandidge <sandidgec@gmail.com>
2015-09-22 21:35:56 -0600

    added admin panel (add businesses) page - started forms

--- pages/adminpanel.php
+++ pages/adminpanel.php
@@ -1,26 +1,34 @@
 <!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<link type="text/css" href="../pages/admin.css" rel="stylesheet"/>
-		</head>
-			<body>
-				<header>
-					<h3>Admin-Panel</h3>
-				</header>
+<html lang="en">
+	<head>
+		<link type="text/css" href="../pages/admin.css" rel="stylesheet"/>
+	</head>
+	<body>
+		<header>
+			<h3>Admin-Panel</h3>
+		</header>
 
-				<form class="form-horizontal" role="form" method="post" action="#">
-					<div class="col-sm-6 col-sm-offset-3">
-						<div class="form-group">
-							<label for="business" class="col-sm-2 control-label"></label>
+		<form class="form-horizontal" role="form" method="post" action="#">
+			<div class="col-sm-6 col-sm-offset-3">
+				<div class="form-group">
+					<label for="Example Business">Business</label>
+					<input type="text" class="form-control" id="business" placeholder="Business">
+				</div>
 
-						</div>
+				<div class="form-group">
+					<label for="category">Category</label>
+					<input type="text" class="form-control" id="category" placeholder="Category">
+				</div>
 
-					</div>
-				</form>
-			</body>
+				<div class="form-group">
+					<label for="category">sub-category</label>
+					<input type="text" class="form-control" id="sub-category" placeholder="sub-category">
+				</div>
+			</div>
 
-
-
+			</div>
+		</form>
+	</body>
 
 
 </html>
