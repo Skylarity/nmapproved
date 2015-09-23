403239d7ee8c95fae2e6eed4068ce743c12abf82
Charles Sandidge <sandidgec@gmail.com>
2015-09-22 21:29:23 -0600

    added admin panel (add businesses) page

---
+++ pages/adminpanel.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<link type="text/css" href="../pages/admin.css" rel="stylesheet"/>
+		</head>
+			<body>
+				<header>
+					<h3>Admin-Panel</h3>
+				</header>
+
+				<form class="form-horizontal" role="form" method="post" action="#">
+					<div class="col-sm-6 col-sm-offset-3">
+						<div class="form-group">
+							<label for="business" class="col-sm-2 control-label"></label>
+
+						</div>
+
+					</div>
+				</form>
+			</body>
+
+
+
+
+
+</html>
