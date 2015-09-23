e33ca99bae77989218fde0819d3c0940be108317
Skyler Rexroad <skyrex1095@gmail.com>
2015-09-23 16:52:07 -0600

    Changed logo to png

--- php/lib/info.php
+++ php/lib/info.php
@@ -4,7 +4,7 @@
 			<div class="col-md-4">
 				<img src="<?php echo $PREFIX ?>img/nma/logo.png" class="img-responsive center-block logo"/>
 			</div>
-			<div class="col-md-8">
+			<div class="col-md-4 col-md-offset-4">
 				<?php require_once($PREFIX . "php/lib/header.php"); ?>
 			</div>
 		</div>
