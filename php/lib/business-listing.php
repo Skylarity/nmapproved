bfbd031e3a487da1ffac32f5db5721f000cc3add
Skyler Rexroad <skyrex1095@gmail.com>
2015-09-23 22:16:34 -0600

    Working on subcategory template, broke it when I switched over to business objects

--- php/lib/business-listing.php
+++ php/lib/business-listing.php
@@ -1,7 +1,28 @@
-<div class="row">
+<div class="row business-listing">
     <div class="col-md-12">
-        <div class="business-listing">
-            <h3><?php echo $business; ?></h3>
+        <div class="col-md-3">
+            <div>Image goes here.</div>
+        </div>
+        <div class="col-md-9">
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h3 class="bus-h3"><?php echo $businessName; ?></h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <p>Description goes here.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <span class="bus-info">(505) 555-555</span>
+                    <br/>
+                    <span class="bus-info">Location St. 1234</span>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- php/lib/subcategory.php
+++ php/lib/subcategory.php
@@ -6,7 +6,12 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 
 /* TESTING */
 
-$_SESSION["businesses"] = ["wow", "these", "are", "businesses", "good", "job", "you", "sure", "did", "it", "wow"];
+$_SESSION["subcategory"] = "bakery";
+$bus1 = new Business(null, "Test 1", "PLace St. 1234", "5055555555", "website.com", "email@website.com", "eat", "bakery");
+$bus2 = new Business(null, "Test 2", "PLace St. 1234", "5057777777", "website.com", "email@website.com", "eat", "bakery");
+$bus3 = new Business(null, "Test 3", "PLace St. 1234", "5059999999", "website.com", "email@website.com", "eat", "bakery");
+var_dump($bus1);
+$_SESSION["businesses"] = [$bus1, $bus2, $bus3];
 
 /* /TESTING */
 
@@ -15,14 +20,14 @@ $_SESSION["businesses"] = ["wow", "these", "are", "businesses", "good", "job", "
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1 class="cat-title"><?php echo "Title"; ?></h1>
+                <h1 class="cat-title"><?php echo ucwords($_SESSION["subcategory"]); ?></h1>
             </div>
         </div>
         <?php
         $businesses = $_SESSION["businesses"];
 
         foreach($businesses as $business) {
-            $business = ucwords($business);
+            $businessName = ucwords($business->getName());
             require($PREFIX . "php/lib/business-listing.php");
         }
         ?>
