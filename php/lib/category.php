ba37d803eae21654af44d8378661b09a8e1d9311
Skyler Rexroad <skyrex1095@gmail.com>
2015-09-25 13:41:12 -0600

    Eat/shop/play links work now

--- php/lib/category.php
+++ php/lib/category.php
@@ -1,40 +1,39 @@
 <?php
 require_once($PREFIX . "php/classes/autoload.php");
 if(session_status() !== PHP_SESSION_ACTIVE) {
-    session_start();
+	session_start();
 }
 
 /* TESTING */
 
-$_SESSION["category"] = "eat";
 $_SESSION["subcategories"] = ["wow", "these", "are", "subcategories", "good", "job", "you", "sure", "did", "it", "wow"];
 
 /* /TESTING */
 
 ?>
 <div class="category">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="cat-title"><?php echo ucwords($_SESSION["category"]); ?></h1>
-            </div>
-        </div>
-        <?php
-        $subcategories = $_SESSION["subcategories"];
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="cat-title"><?php echo ucwords($_GET["category"]); ?></h1>
+			</div>
+		</div>
+		<?php
+		$subcategories = $_SESSION["subcategories"];
 
-        $i = 1;
-        echo "<div class=\"row\">" . PHP_EOL;
-        foreach($subcategories as $subcategory) {
-            $subcategory = ucwords($subcategory);
-            if($i % 4 === 0) {
-                echo "</div>" . PHP_EOL;
-                echo "<div class=\"row\">" . PHP_EOL;
-            }
-            echo "<div class=\"col-md-3\">" . PHP_EOL;
-            require($PREFIX . "php/lib/subcategory-listing.php");
-            echo "</div>" . PHP_EOL;
-        }
-        ?>
-    </div>
+		$i = 1;
+		echo "<div class=\"row\">" . PHP_EOL;
+		foreach($subcategories as $subcategory) {
+			$subcategory = ucwords($subcategory);
+			if($i % 4 === 0) {
+				echo "</div>" . PHP_EOL;
+				echo "<div class=\"row\">" . PHP_EOL;
+			}
+			echo "<div class=\"col-md-3\">" . PHP_EOL;
+			require($PREFIX . "php/lib/subcategory-listing.php");
+			echo "</div>" . PHP_EOL;
+		}
+		?>
+	</div>
 </div>
 </div>
--- php/lib/eat-shop-play.php
+++ php/lib/eat-shop-play.php
@@ -1,17 +1,17 @@
 <div class="eat-shop-play">
 	<div class="container">
 		<div class="row">
-			<a href="#">
+			<a href="<?php echo $PREFIX; ?>category/?category=eat">
 				<div class="col-md-4 category-esp eat">
 					<h2>Eat.</h2>
 				</div>
 			</a>
-			<a href="#">
+			<a href="<?php echo $PREFIX; ?>category/?category=shop">
 				<div class="col-md-4 category-esp shop">
 					<h2>Shop.</h2>
 				</div>
 			</a>
-			<a href="#">
+			<a href="<?php echo $PREFIX; ?>category/?category=play">
 				<div class="col-md-4 category-esp play">
 					<h2>Play.</h2>
 				</div>
